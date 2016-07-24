8879d3314ed570778c06e39b5b0a72cdd0e3b48a
Jake Lee <louisoel@gmail.com>
2016-07-24 02:51:04 -0400

    pseudoclass background
    pseudoclass background image was still jumping, attempted fix.

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 $(document).ready(function() {
 	//credit: http://stackoverflow.com/questions/24944925/background-image-jumps-when-address-bar-hides-ios-android-mobile-chrome
     var bg = jQuery("#div1, #div2");
-	jQuery(window).resize("resizeBackground");
+	$(window).resize("resizeBackground");
 	function resizeBackground() {
+		var newHeight = jQuery(window).height() + 60;
+		var newWidth = jQuery(window).width();
     	bg.height(jQuery(window).height() + 60);
+    	document.styleSheets[0].addRule('.front:before', 'height: ' + newHeight + "; width: " + newWidth + ";");
 	}
-	if($(window).width() <= 768){
-		resizeBackground();
-	}
+	console.log($(window).width());
 });
