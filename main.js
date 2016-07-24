067fd61209915cc735fb1e58ccafbe572b514485
Jake Lee <louisoel@gmail.com>
2016-07-24 19:53:03 -0400

    Mobile address bar fix attempt
    Still trying to fix background image jump in android chrome browsers
    due to sliding address bars

--- main.js
+++ main.js
@@ -1,14 +1,17 @@
 $(document).ready(function() {
 	//credit: http://stackoverflow.com/questions/24944925/background-image-jumps-when-address-bar-hides-ios-android-mobile-chrome
-    var bg = jQuery("#div1, #div2");
+    var bg = $("#div1, #div2");
 	$(window).resize("resizeBackground");
 	function resizeBackground() {
 		var newHeight = jQuery(window).height() + 60;
 		var newWidth = jQuery(window).width();
     	bg.height(jQuery(window).height() + 60);
-    	document.styleSheets[0].addRule('.front:before', 'height: ' + newHeight + "; width: " + newWidth + ";");
+    	console.log(document.styleSheets);
+    	var rule1 = ".front::before {width: " + newWidth + "px; height: " + newHeight +"px }";
+    	console.log(rule1);
+    	document.styleSheets[1].insertRule(rule1, 0);
 	}
-	console.log($(window).width());
+
 	if($(window).width() < 768){
 		resizeBackground();
 	}
