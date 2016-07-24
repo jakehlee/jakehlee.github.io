7043b176f238f8d198156e57f577ff51436bbdca
Jake Lee <louisoel@gmail.com>
2016-07-24 02:31:52 -0400

    Javascript implementation for bg sizing
    Again, attempting to fix “jumping” issues caused by sliding address
    bar. <3 stackoverflow

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 $(document).ready(function() {
-    $("#sidebar-toggle").click(function(event) {
-        event.preventDefault();
-        $("#wrapper").toggleClass("toggled");
-    });
+	//credit: http://stackoverflow.com/questions/24944925/background-image-jumps-when-address-bar-hides-ios-android-mobile-chrome
+    var bg = jQuery("#div1, #div2");
+	jQuery(window).resize("resizeBackground");
+	function resizeBackground() {
+    	bg.height(jQuery(window).height() + 60);
+	}
+	if($(window).width() <= 768){
+		resizeBackground();
+	}
 });
