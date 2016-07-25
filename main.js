f77a4c00684d63569eecdbc2b6a3b2d0f3a8fbd2
Jake Lee <louisoel@gmail.com>
2016-07-25 14:57:36 -0400

    Testing consistency across android/ios

--- main.js
+++ main.js
@@ -1,16 +1,19 @@
+
 $(document).ready(function() {
 	//credit: http://stackoverflow.com/questions/24944925/background-image-jumps-when-address-bar-hides-ios-android-mobile-chrome
     var bg = $("#div1");
-	$(window).resize("resizeBackground");
-	function resizeBackground() {
+	$(window).resize("resizeIndexBackground");
+	function resizeIndexBackground() {
 		var newHeight = jQuery(window).height() + 60;
 		var newWidth = jQuery(window).width();
     	bg.height(jQuery(window).height() + 60);
-    	var rule1 = ".front::before {width: " + newWidth + "px !important; height: " + newHeight +"px !important}";
+    	var rule1 = "#div1::before {width: " + newWidth + "px !important; height: " + newHeight +"px !important}";
     	document.styleSheets[1].insertRule(rule1, 1);
+    	var rule2 = ".static-full {width: " + newWidth + "px !important; height: " + newHeight + "px !important}";
+    	document.styleSheets[1].insertRule(rule2, 2);
 	}
 
 	if($(window).width() < 768){
-		resizeBackground();
+		resizeIndexBackground();
 	}
 });
