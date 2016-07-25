9d3765e2d76aa55fac287d36066426579d0dde99
Jake Lee <louisoel@gmail.com>
2016-07-24 20:51:03 -0400

    Re-enabling back ground on desktop

--- main.js
+++ main.js
@@ -1,15 +1,13 @@
 $(document).ready(function() {
 	//credit: http://stackoverflow.com/questions/24944925/background-image-jumps-when-address-bar-hides-ios-android-mobile-chrome
-    var bg = $("#div1, #div2");
+    var bg = $("#div1");
 	$(window).resize("resizeBackground");
 	function resizeBackground() {
 		var newHeight = jQuery(window).height() + 60;
 		var newWidth = jQuery(window).width();
     	bg.height(jQuery(window).height() + 60);
-    	console.log(document.styleSheets);
-    	var rule1 = ".front::before {width: " + newWidth + "px; height: " + newHeight +"px }";
-    	console.log(rule1);
-    	document.styleSheets[1].insertRule(rule1, 0);
+    	var rule1 = ".front::before {width: " + newWidth + "px !important; height: " + newHeight +"px !important}";
+    	document.styleSheets[1].insertRule(rule1, 1);
 	}
 
 	if($(window).width() < 768){
